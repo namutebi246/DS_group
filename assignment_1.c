5b2ae865a737f78896a7186ae8a539afb80ea93e
raj-jadhav <jadxur@gmail.com>
2025-03-03 22:18:51 +0300

    Fixed typo on assignment keyword used in the assignment_1.c file

--- assignment_1.c
+++ assignment_1.c
@@ -9,14 +9,14 @@ int average();
 
 int main()
 {
-    int n_assigment, n_course_work, n_mid_term, n_endOfTerm;
-    int m_assigment, m_course_work, m_mid_term, m_endOfTerm ;
-    int x_assigment, x_course_work, x_mid_term, x_endOfTerm;
+    int n_assignment, n_course_work, n_mid_term, n_endOfTerm;
+    int m_assignment, m_course_work, m_mid_term, m_endOfTerm ;
+    int x_assignment, x_course_work, x_mid_term, x_endOfTerm;
     
     // Marks for physics
     printf("Enter physics marks\n");
-    printf("Enter marks for assigment\n");
-    scanf("%d", &n_assigment);
+    printf("Enter marks for assignment\n");
+    scanf("%d", &n_assignment);
     printf("Enter marks forcourse_work\n");
     scanf("%d",&n_course_work);
     printf("Enter marks for mid_term\n");
@@ -24,9 +24,9 @@ int main()
     printf("Enter marks for end of term\n");
     scanf("%d", &n_endOfTerm);
     // Marks for chemistry
-    printf("Enter chemistry marks");
-    printf("Enter marks for assigment\n");
-    scanf("%d", &m_assigment);
+    printf("Enter chemistry marks\n");
+    printf("Enter marks for assignment\n");
+    scanf("%d", &m_assignment);
     printf("Enter marks forcourse_work\n");
     scanf("%d",&m_course_work);
     printf("Enter marks for mid_term\n");
@@ -35,8 +35,8 @@ int main()
     scanf("%d", &m_endOfTerm);
     // Marks for math
     printf("Enter math marks\n");
-    printf("Enter marks for assigment\n");
-    scanf("%d", &x_assigment);
+    printf("Enter marks for assignment\n");
+    scanf("%d", &x_assignment);
     printf("Enter marks forcourse_work\n");
     scanf("%d",&x_course_work);
     printf("Enter marks for mid_term\n");
@@ -45,9 +45,9 @@ int main()
     scanf("%d", &x_endOfTerm);
 
 
-    int sum = physics(n_assigment, n_course_work, n_mid_term, n_endOfTerm); 
-    int n = chemistry(m_assigment, m_course_work, m_mid_term,m_endOfTerm);
-    int m = math(x_assigment, x_course_work, x_mid_term, x_endOfTerm);
+    int sum = physics(n_assignment, n_course_work, n_mid_term, n_endOfTerm); 
+    int n = chemistry(m_assignment, m_course_work, m_mid_term,m_endOfTerm);
+    int m = math(x_assignment, x_course_work, x_mid_term, x_endOfTerm);
     int marks = average(sum, n, m);
     printf("\nTotal Marks:\nPhysics: %d\nChemistry: %d\nMath: %d\n", sum, n, m);
     printf("Average marks: %d\n",marks);
@@ -57,18 +57,18 @@ int main()
 
     return 0;
 }
-int physics(int assigment, int course_work, int mid_term, int endOfTerm)
+int physics(int assignment, int course_work, int mid_term, int endOfTerm)
 {
-    int total = assigment + course_work + mid_term + endOfTerm;
+    int total = assignment + course_work + mid_term + endOfTerm;
     return total;
 }
-int chemistry(int assigment, int course_work, int mid_term, int endOfTerm){
-    int total = assigment + course_work + mid_term + endOfTerm;
+int chemistry(int assignment, int course_work, int mid_term, int endOfTerm){
+    int total = assignment + course_work + mid_term + endOfTerm;
     return total;
 
 }
-int math(int assigment, int course_work, int mid_term, int endOfTerm){
-    int total = assigment + course_work + mid_term + endOfTerm;
+int math(int assignment, int course_work, int mid_term, int endOfTerm){
+    int total = assignment + course_work + mid_term + endOfTerm;
     return total;
 }
 int average(int physics, int chemistry, int math){
