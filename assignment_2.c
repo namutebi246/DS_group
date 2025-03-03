9672cd04314f68b432d546c6b606f3010e1c951d
katsomar <153194531+katsomar@users.noreply.github.com>
2025-03-03 23:45:27 +0300

    Modified assignment_2.c removed unwanted code

--- assignment_2.c
+++ assignment_2.c
@@ -42,35 +42,3 @@ int main(){
 
     return 0;
 }
-
-
-/*
-
-int main(){
-    char xWildAnimals[50];
-    char nDomesticAnimals[50];
-    int numX;
-    int numY;
-    int i;
-    int j;
-
-    printf("Please enter The number of wild animals: \n");
-    scanf("%d", &numX); 
-    getchar(); 
-    for(i = 1; i <= numX; i++){
-        printf("Wild animal %d\n", i);
-        fgets(xWildAnimals, 50, stdin);  
-
-    }
-
-    printf("Please enter The number of domestic animals: \n");
-    scanf("%d", &numY); 
-    getchar(); 
-    for(j = 1; j <= numY; j++){
-        printf("Domestic animal %d\n", j);
-        fgets(nDomesticAnimals, 50, stdin);  
-
-    }
-
-*/
-
