e9a135752fdf190ed93c429016d6d5468cfe656f
Raj-Jadhav <jadxur@gmail.com>
2025-03-20 09:23:20 +0300

    Update merge_array.c

--- Introduction/merge_array.c
+++ Introduction/merge_array.c
@@ -1,43 +1,47 @@
-#include <stdio.h>
+#include <stdio.h>  
 
-
-int main(){
+int main() {
+    // Arrays to store wild and domestic animal names (50 names, each up to 49 characters + null terminator)
     char xWildAnimals[50][50];
     char nDomesticAnimals[50][50];
-    int numX, numY;
-    int i, j;
 
+    int numX, numY;  // Variables to store the number of wild and domestic animals
+    int i, j;        // Loop counters
 
+    // Ask the user for the number of wild animals
     printf("Please enter the number of wild animals: \n");
     scanf("%d", &numX);
-    getchar();
+    getchar(); // Consume the newline character left in the input buffer
 
-
-    for(i = 0; i < numX; i++){
+    // Loop to get wild animal names
+    for (i = 0; i < numX; i++) {
         printf("Wild animal %d: \n", i + 1);
-        fgets(xWildAnimals[i], 50, stdin);
+        fgets(xWildAnimals[i], 50, stdin); // Read the wild animal name (including spaces)
     }
 
+    // Ask the user for the number of domestic animals
     printf("Please enter the number of domestic animals: \n");
     scanf("%d", &numY);
-    getchar();
+    getchar(); // Consume the newline character left in the input buffer
 
-    for(j = 0; j < numY; j++){
+    // Loop to get domestic animal names
+    for (j = 0; j < numY; j++) {
         printf("Domestic animal %d: \n", j + 1);
-        fgets(nDomesticAnimals[j], 50, stdin);
+        fgets(nDomesticAnimals[j], 50, stdin); // Read the domestic animal name (including spaces)
     }
 
-        // Print N + X animals in a single list
-         // Print all animals in a single list
+    // Print all animals in a single list
     printf("\nList of all animals:\n");
 
-    for(i = 0; i < numX; i++) {
-        printf("%d. %s\n", i + 1, xWildAnimals[i]);
+    // Print wild animals first
+    for (i = 0; i < numX; i++) {
+        printf("%d. %s", i + 1, xWildAnimals[i]); // Printing each wild animal with its index
     }
 
-    for(j = 0; j < numY; j++) {
-        printf("%d. %s\n", numX + j + 1, nDomesticAnimals[j]);
+    // Print domestic animals, continuing numbering from wild animals
+    for (j = 0; j < numY; j++) {
+        printf("%d. %s", numX + j + 1, nDomesticAnimals[j]); // Printing each domestic animal with its index
     }
 
- return 0;
+    return 0; 
 }
