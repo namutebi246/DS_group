9d436d8ec2ca4f63b01f251929d5df34cd00cd9f
Raj-Jadhav <jadxur@gmail.com>
2025-03-20 09:06:34 +0300

    Update merge_array.c

--- Introduction/merge_array.c
+++ Introduction/merge_array.c
@@ -2,41 +2,42 @@
 
 
 int main(){
-    char xWildAnimals[50][50];  
-    char nDomesticAnimals[50][50];  
-    int numX, numY;  
+    char xWildAnimals[50][50];
+    char nDomesticAnimals[50][50];
+    int numX, numY;
     int i, j;
 
-    
-    printf("Please enter the number of wild animals: \n");
-    scanf("%d", &numX); 
-    getchar(); 
 
-    
+    printf("Please enter the number of wild animals: \n");
+    scanf("%d", &numX);
+    getchar();
+
+
     for(i = 0; i < numX; i++){
         printf("Wild animal %d: \n", i + 1);
-        fgets(xWildAnimals[i], 50, stdin); 
+        fgets(xWildAnimals[i], 50, stdin);
     }
 
     printf("Please enter the number of domestic animals: \n");
-    scanf("%d", &numY); 
-    getchar(); 
+    scanf("%d", &numY);
+    getchar();
 
     for(j = 0; j < numY; j++){
         printf("Domestic animal %d: \n", j + 1);
-        fgets(nDomesticAnimals[j], 50, stdin);  
+        fgets(nDomesticAnimals[j], 50, stdin);
     }
 
-    
-    printf("\nWild Animals & Domestic Animals List:\n");
-    for(i = 0; i < numX; i++){
-        printf("%d. %s", i + 1, xWildAnimals[i]);  
+        // Print N + X animals in a single list
+         // Print all animals in a single list
+    printf("\nList of all animals:\n");
+
+    for(i = 0; i < numX; i++) {
+        printf("%d. %s\n", i + 1, xWildAnimals[i]);
     }
 
-        
-    for(j = 0; j < numY; j++){
-        printf("%d. %s", j + 1, nDomesticAnimals[j]);  
+    for(j = 0; j < numY; j++) {
+        printf("%d. %s\n", numX + j + 1, nDomesticAnimals[j]);
     }
 
-    return 0;
+ return 0;
 }
