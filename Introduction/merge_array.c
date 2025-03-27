ba7b4c52e9d737f15fdbcad6e301cd35b47ae606
ronald-nsimbi <ronie.nsimbi@gmail.com>
2025-03-27 18:44:52 +0300

    Edited merge_array.c file; Enabled dynamic memory allocation with proper error handling and memory management, corrected the code to allow proper merging before printing the merged array.

--- Introduction/merge_array.c
+++ Introduction/merge_array.c
@@ -1,42 +1,51 @@
-#include <stdio.h>
+#include <stdio.h>  
 
+int main() {
+    int numX, numY, i;  
 
-int main(){
-    char xWildAnimals[50][50];  
-    char nDomesticAnimals[50][50];  
-    int numX, numY;  
-    int i, j;
-
-    
+    // Ask the user for the number of wild animals
     printf("Please enter the number of wild animals: \n");
-    scanf("%d", &numX); 
-    getchar(); 
+    scanf("%d", &numX);
+    getchar(); // Consume the newline character left in the input buffer
+    char xWildAnimals[numX][50];
 
-    
-    for(i = 0; i < numX; i++){
+    // Get wild animal names
+    for (i = 0; i < numX; i++) {
         printf("Wild animal %d: \n", i + 1);
-        fgets(xWildAnimals[i], 50, stdin); 
+        fgets(xWildAnimals[i], 50, stdin);
     }
 
+    // Ask the user for the number of domestic animals
     printf("Please enter the number of domestic animals: \n");
-    scanf("%d", &numY); 
-    getchar(); 
+    scanf("%d", &numY);
+    getchar(); // Consume the newline character left in the input buffer
+    char nDomesticAnimals[numY][50];
 
-    for(j = 0; j < numY; j++){
-        printf("Domestic animal %d: \n", j + 1);
-        fgets(nDomesticAnimals[j], 50, stdin);  
+    // Get domestic animal names
+    for (i = 0; i < numY; i++) {
+        printf("Domestic animal %d: \n", i + 1);
+        fgets(nDomesticAnimals[i], 50, stdin);
     }
 
-    
-    printf("\nWild Animals & Domestic Animals List:\n");
-    for(i = 0; i < numX; i++){
-        printf("%d. %s", i + 1, xWildAnimals[i]);  
+    // Merge both arrays into one
+    int totalAnimals = numX + numY;
+    char mergedAnimals[totalAnimals][50];
+
+    // Copy wild animals to merged array
+    for (i = 0; i < numX; i++) {
+        snprintf(mergedAnimals[i], 50, "%s", xWildAnimals[i]);
     }
 
-        
-    for(j = 0; j < numY; j++){
-        printf("%d. %s", j + 1, nDomesticAnimals[j]);  
+    // Copy domestic animals to merged array
+    for (i = 0; i < numY; i++) {
+        snprintf(mergedAnimals[numX + i], 50, "%s", nDomesticAnimals[i]);
     }
 
-    return 0;
+    // Print merged array
+    printf("\nList of all animals:\n");
+    for (i = 0; i < totalAnimals; i++) {
+        printf("%d. %s", i + 1, mergedAnimals[i]);
+    }
+
+    return 0; 
 }
